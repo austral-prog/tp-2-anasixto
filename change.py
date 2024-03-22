458b42d3ca6f985b34411a108e47387adf347dae
anasixto <162721927+anasixto@users.noreply.github.com>
2024-03-22 12:51:05 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,15 @@
 def change():
     expense = 23.75
     money = 100
+    vuelto = money - expense
+    
+    print("Ingresar gasto:")
+    print(expense)
+    print("Dinero recibido")
+    print(money)
+    print()
+    print("Vuelto")
+    print()
+    print("Pesos:")
+    print(int(vuelto))
+    print(float(vuelto*100))
