949a017a622e11dabf64c62ca6397d49f48edd1f
anasixto <162721927+anasixto@users.noreply.github.com>
2024-03-30 19:09:50 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -3,4 +3,3 @@ def earth():
     y = "Barbados"
     print("The result of Bangladesh comes first in the dictionary than Barbados is" f"{x > y}.")
     print("The result of Barbados comes first in the dictionary than Bangladesh is" f"{x < y}.")
-earth()
